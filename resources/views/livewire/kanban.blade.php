c019e2e61a79001dc2cd484eb8e1360253d4eaa2
alexandr <alexandr87m@gmail.com>
2023-01-25 22:04:41 +0500

    Fix card moving width

--- resources/views/livewire/kanban.blade.php
+++ resources/views/livewire/kanban.blade.php
@@ -32,7 +32,7 @@
             <div wire:sortable-group.item-group="{{ $group->id }}" class="flex flex-col pb-2 overflow-auto">
 
                 @foreach($group->cards as $card)
-                <div wire:key="task-{{ $card->id }}" wire:sortable-group.item="{{ $card->id }}" class="relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100" draggable="true">
+                <div wire:key="task-{{ $card->id }}" wire:sortable-group.item="{{ $card->id }}" class="relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100 w-72" draggable="true">
                     <button wire:click="deleteCard({{ $card->id }})" class="absolute top-0 right-0 flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-red-700 rounded hover:bg-red-200 hover:text-red-800 group-hover:flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
