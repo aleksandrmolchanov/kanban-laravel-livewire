78387a5584f5e4ecbc5b099599b89354c5d88af4
alexandr <alexandr87m@gmail.com>
2023-01-25 22:04:41 +0500

    Fix card moving width

--- resources/views/livewire/kanban.blade.php
+++ resources/views/livewire/kanban.blade.php
@@ -42,7 +42,7 @@
             <div wire:sortable-group.item-group="{{ $group->id }}" class="flex flex-col pb-2 overflow-auto">
 
                 @foreach($group->cards as $card)
-                <div wire:key="task-{{ $card->id }}" wire:sortable-group.item="{{ $card->id }}" class="relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100" draggable="true">
+                <div wire:key="task-{{ $card->id }}" wire:sortable-group.item="{{ $card->id }}" class="relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100 w-72" draggable="true">
                     <button wire:click="$emit('openModal', 'edit-card', {{ json_encode(["id" => $card->id]) }})"  wire:mousedown="$emit('openModal', 'edit-card', {{ json_encode(["id" => $card->id]) }})" class="absolute top-0 right-[24px] flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-indigo-800 rounded hover:bg-indigo-100 hover:text-indigo-700 group-hover:flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[1.1rem] h-[1.1rem]">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
